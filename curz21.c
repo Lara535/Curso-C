10da762587ccc214b9b1cc52edff29a502089b36
Lara Abreu <lara.abreu@sempreceub.com>
2025-06-12 11:21:48 -0300

    Add files via upload

---
+++ curz21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// questão 21
+int main(){
+   int i, j;
+ 
+    for(j = 1; j <= 10; j++){ // isso vc usa pra vezes
+        for(i = 1; i <= 20; i++)
+            printf("*");
+    printf("\n");
+    }
+
+    return 0;
+}
